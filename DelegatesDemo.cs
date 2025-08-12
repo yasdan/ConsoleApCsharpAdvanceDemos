199dfc42af3c46437d3be0bf576c42ac1d12778c
Abdul <yasdanis@gmail.com>
2025-08-12 09:42:52 +0530

    Add project files.

---
+++ DelegatesDemo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApCsharpAdvanceDemos
+{
+    // delegates are type-safe function pointers that can be used to
+    // encapsulate a method with a specific signature.
+    public delegate void Notify(string message);
+
+    public delegate void CalculateDelegate(int x, int y);
+
+    internal class DelegatesDemo
+    {
+
+        public void Sum(int x, int y)
+        {
+            Console.WriteLine($"Sum: {x + y}");
+        }
+
+        public void Multiply(int x, int y)
+        {
+            Console.WriteLine($"Product: {x * y}");
+        }
+
+        public void Divide(int x, int y)
+        {
+            if (y == 0)
+            {
+                Console.WriteLine("Cannot divide by zero.");
+                return;
+            }
+            Console.WriteLine($"Division: {x / y}");
+        }
+
+        public void Subtract(int x, int y)
+        {
+            Console.WriteLine($"Difference is :{x - y}");
+
+        }
+
+
+
+        // Declare a delegate type Notify that takes a string parameter.
+        public event Notify NotifyEvent;
+        public void TriggerEvent(string message)
+        {
+            NotifyEvent?.Invoke(message);
+           
+        }
+
+        // The NotifyEvent is an event of type Notify delegate.
+        public void Subscribe(Notify handler)
+        {
+            NotifyEvent += handler;
+        }
+        public void Unsubscribe(Notify handler)
+        {
+            NotifyEvent -= handler;
+            Console.WriteLine("Unsubscribed from the event.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApCsharpAdvanceDemos;
+
+Console.WriteLine("Hello, World! Welcome to Advance C#.Net Topics");
+
+DelegatesExample();
+
+static void DelegatesExample()
+{
+    Console.WriteLine("Delegates Example");
+
+    CalculateDelegate calculateDelegate; // declare a delegate variable
+
+    // This is a simple example of using delegates in C#.
+    DelegatesDemo delegatesDemo = new DelegatesDemo();
+
+    calculateDelegate = delegatesDemo.Sum;
+    // Using the delegate to call methods directly
+    calculateDelegate(20, 30);
+    calculateDelegate += delegatesDemo.Multiply;
+
+    calculateDelegate(15, 10);
+    // Removing a method from the delegate invocation list
+    calculateDelegate -= delegatesDemo.Multiply;
+    calculateDelegate(50, 25);
+    // Invoking the delegate
+    calculateDelegate.Invoke(5, 10);
+    calculateDelegate += delegatesDemo.Divide;
+    calculateDelegate(100, 20);
+
+    // Using delegates with events
+    delegatesDemo.Subscribe(message => Console.WriteLine($"Received message: {message}"));
+
+    delegatesDemo.TriggerEvent("This is a test message after subscribing!");
+    delegatesDemo.Unsubscribe(message => Console.WriteLine($"Received message: {message}"));
+}
+
